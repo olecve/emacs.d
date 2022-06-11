d59a50ae0bdcb91ac29f700d60dfb0cf380bb768
Oleg Tsvetkov <olecve@gmail.com>
2022-06-11 10:41:58 +0300

    Start new config from scratch

--- init.el
+++ init.el
@@ -1,10 +1,9 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;(package-initialize)
+(setq inhibit-startup-message t)
 
-(require 'org)
-(org-babel-load-file
- (expand-file-name "settings.org"
-                   user-emacs-directory))
+;; Turn off unneeded UI elements
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Display line number in every buffer
+(global-display-line-numbers-mode 1)
