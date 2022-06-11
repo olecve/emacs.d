14100b140cb9a348e8bc3e2627027c74454d6a24
Oleg Tsvetkov <olecve@gmail.com>
2022-06-11 11:30:26 +0300

    Add basic settings

--- init.el
+++ init.el
@@ -7,3 +7,25 @@
 
 ;; Display line number in every buffer
 (global-display-line-numbers-mode 1)
+
+(load-theme 'modus-vivendi t)
+
+(setq recentf-save-file (concat user-emacs-directory ".recentf"))
+(setq recentf-max-menu-items 25)
+(recentf-mode 1)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+(setq history-length 25)
+(savehist-mode 1)
+
+;; Remember and restore the last cursor location of opened files
+(save-place-mode 1)
+
+;; Don't pop up UI dialogs when promting
+(setq use-dialog-box nil)
+
+;; Revert the buffer when the underlying file has changed
+(global-auto-revert-mode 1)
+
+;; Revert Dired and other buffers
+(setq global-auto-revert-non-file-buffers t)
