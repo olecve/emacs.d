299252dde83b1e6a413971d7bf0c5feb33294bd1
Oleg Tsvetkov <olecve@gmail.com>
2026-04-20 21:29:49 +0300

    Reduce startup time via GC hack, lazy org-babel tangle, and cleanup
    - Defer GC to most-positive-fixnum during init, restore to 16MB after
    - Only load org + tangle settings.org when settings.el is stale; otherwise load settings.el directly
    - Remove package-refresh-contents call on every startup
    - Remove misleading :defer t on doom-themes (theme is loaded eagerly anyway)

--- early-init.el
+++ early-init.el
@@ -5,6 +5,14 @@
 
 (setq package-enable-at-startup nil)
 
+;; Defer GC during startup; restore to a sane value after init completes.
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 16 1024 1024)
+                  gc-cons-percentage 0.1)))
+
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
--- init.el
+++ init.el
@@ -1,10 +1,7 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;(package-initialize)
-
-(require 'org)
-(org-babel-load-file
- (expand-file-name "settings.org"
-                   user-emacs-directory))
+(let* ((org-file (expand-file-name "settings.org" user-emacs-directory))
+       (el-file  (expand-file-name "settings.el"  user-emacs-directory)))
+  (when (or (not (file-exists-p el-file))
+            (file-newer-than-file-p org-file el-file))
+    (require 'org)
+    (org-babel-tangle-file org-file el-file "emacs-lisp"))
+  (load-file el-file))
